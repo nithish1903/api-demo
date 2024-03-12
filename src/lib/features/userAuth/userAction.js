eac4d0d4d95e0b6542d81b5a6150ca3b0282cbcc
suriya1113450 <suriya@adcanopus.com>
2024-03-12 18:43:56 +0530

    change in user action

--- src/lib/features/userAuth/userAction.js
+++ src/lib/features/userAuth/userAction.js
@@ -4,19 +4,52 @@ import axios from "axios"
 
 
 
-export const userLogin = createAsyncThunk("user/login", async (req, thunkApi) => {
-    try {
-        const response = await axios.post("http://localhost:9024/v1/user/login", req.formData)
+// export const userLogin = createAsyncThunk("user/login", async (req, thunkApi) => {
+//     try {
+//         const response = await axios.post("http://localhost:9024/v1/user/login", req.formData)
+//         if (response.status === 200 && response.data.statuscode===200) {
+//             const resData =  response.data
+//             if(resData && resData.data && Object.keys(resData.data).length>0){
+//                 const data = resData.data
+//                 console.log(response.headers);
+
+//                 console.log(response.headers['content-type']);
+//                 console.log(response.headers['cache-control']);
+//                 console.log(response.headers['expires']);
+//                 console.log(response.headers['pragma']);
+//                 req.resolve()
+//                 return data
+//             }
+//         }
+//     } catch (error) {
+//         return thunkApi.rejectWithValue(error)
+//     }
+// })
+
+
+export const userLogin = createAsyncThunk("user/login", (req, thunkApi) => {
+    axios.post("http://localhost:9024/v1/user/login", req.formData,  {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    .then((response)=>{
+        //  // Access Set-Cookie header
+        console.log(JSON.stringify(response));
+        const sessionCookie = response.headers['set-cookie'];
+        console.log("---sessionCookie---")
+        console.log(sessionCookie);
+
         if (response.status === 200 && response.data.statuscode===200) {
             const resData =  response.data
             if(resData && resData.data && Object.keys(resData.data).length>0){
                 const data = resData.data
-                setCookiesNext(data)
                 req.resolve()
                 return data
             }
         }
-    } catch (error) {
+    })
+    .catch((error)=>{
         return thunkApi.rejectWithValue(error)
-    }
+    })
 })
